821c64b4b61d1c577b2550095c809b49ce7f33f8
Vishal Pasi <vishalnpasi@gmail.com>
2022-10-10 16:47:15 +0530

    some update in controller.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+const createUser = async function(req , res){
+
+}
+
+const loginUser = async function(req , res){
+
+}
+
+const getUser = async function(req , res){
+
+}
+const updateUser = async function(req , res){
+
+}
+
+module.exports = {createUser , loginUser , getUser , updateUser}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
-// const userController = require('../controllers/userController')
-// const bookController = require('../controllers/bookController')
-// const reviewController = require('../controllers/reviewController')
-// const mid = require('../middleware/auth')
+const userController = require('../controllers/userController')
+
+// User APIs....
+
+router.post('/register',userController.createUser)
+
+router.post('/login',userController.loginUser)
+
+router.post('/user/:userId/profile',userController.getUser)
+
+router.post('/user/:userId/profile',userController.updateUser)
+
 
 
 router.use(function(req,res){
